57ec594cc4bb1d3d752763cdb922d7a5c3097e70
Oassam <ra65135@uem.br>
2016-02-14 11:40:01 -0200

    Refatorando teste unitário ApartamentoControllerTest

--- test/br/com/condominio/controller/ApartamentoControllerTest.java
+++ test/br/com/condominio/controller/ApartamentoControllerTest.java
@@ -41,11 +41,18 @@ public class ApartamentoControllerTest {
 
   @After
   public void tearDown() {
+    System.out.println("Removendo Condômino de teste...");
+    removerCondominoTeste();
+    System.out.println("Condômino de teste removido...");
+    System.out.println("Removendo Condomínio de teste...");
+    removerCondominioTeste();
+    System.out.println("Condominio de teste removido.");
   }
 
   @Test
   public void testeAdicionarApartamento_Erro_DataInvalida() throws ParseException {
     System.out.println("adicionarApartamento_Erro_DataInvalida");
+    System.out.println("Criando novo apartamento...");
     Apartamento apartamento = new Apartamento();
     apartamento.setAtivo(true);
     apartamento.setBloco("BL99");
@@ -54,16 +61,24 @@ public class ApartamentoControllerTest {
     apartamento.setDataCadastro(formater.parse("123"));
     apartamento.setId(Long.parseLong("1"));
     apartamento.setNumero("10000");
+    System.out.println("Novo apartamento criado...");
+    System.out.println("Instanciando controlador...");
     ApartamentoController instance = new ApartamentoController();
+    System.out.println("Controlador instanciado...");
     instance.setApartamento(apartamento);
+    System.out.println("Persistindo novo apartamento...");
     String result = instance.manutencaoApartamento();
+    System.out.println("Novo apartamento persistido...");
+    System.out.println("Removendo apartamento persistido...");
     instance.removerEntidade(apartamento);
+    System.out.println("Apartamento removido.");
     assertEquals(URL_CONSULTA, result);
   }
 
   @Test
   public void testeAdicionarApartamento_OK_SemCondominio_SemCondomino() {
     System.out.println("adicionarApartamento_OK_SemCondominio_SemCondomino");
+    System.out.println("Criando novo apartamento...");
     Apartamento apartamento = new Apartamento();
     apartamento.setAtivo(true);
     apartamento.setBloco("BL99");
@@ -71,16 +86,24 @@ public class ApartamentoControllerTest {
     apartamento.setCondomino(null);
     apartamento.setDataCadastro(new Date());
     apartamento.setNumero("100");
+    System.out.println("Novo apartamento criado...");
+    System.out.println("Instanciando controlador...");
     ApartamentoController instance = new ApartamentoController();
+    System.out.println("Controlador instanciado...");
     instance.setApartamento(apartamento);
+    System.out.println("Persistindo novo apartamento...");
     String result = instance.manutencaoApartamento();
+    System.out.println("Novo apartamento persistido...");
+    System.out.println("Removendo apartamento persistido...");
     instance.removerEntidade(apartamento);
+    System.out.println("Apartamento removido.");
     assertEquals(URL_CONSULTA, result);
   }
 
   @Test
   public void testeAdicionarApartamento_OK_ComCondominio_ComCondomino() {    
-    System.out.println("adicionarApartamento_OK_ComCondominio_ComCondomino");    
+    System.out.println("adicionarApartamento_OK_ComCondominio_ComCondomino");   
+    System.out.println("Criando novo apartamento..."); 
     Apartamento apartamento = new Apartamento();
     apartamento.setAtivo(true);
     apartamento.setBloco("BL01");
@@ -88,10 +111,17 @@ public class ApartamentoControllerTest {
     apartamento.setCondomino(getCondomino());
     apartamento.setDataCadastro(new Date());
     apartamento.setNumero("101");
+    System.out.println("Novo apartamento criado...");
+    System.out.println("Instanciando controlador...");
     ApartamentoController instance = new ApartamentoController();
+    System.out.println("Controlador instanciado...");
     instance.setApartamento(apartamento);
+    System.out.println("Persistindo novo apartamento...");
     String result = instance.manutencaoApartamento();
+    System.out.println("Novo apartamento persistido...");
+    System.out.println("Removendo apartamento persistido...");
     instance.removerEntidade(apartamento);
+    System.out.println("Apartamento removido.");
     assertEquals(URL_CONSULTA, result);
   }
 
@@ -120,7 +150,7 @@ public class ApartamentoControllerTest {
     if (condominoDAO.getLista("Massao").isEmpty()) {
     condomino.setApartamento(null);
     condomino.setAtivo(true);
-    condomino.setCPF("00000000000000");
+    condomino.setCPF("72586714202");
     condomino.setCondominio(getCondominio());
     condomino.setNome("Massao");
     condomino.setRG("111111111");
@@ -130,6 +160,22 @@ public class ApartamentoControllerTest {
     return condomino;
     } else{
       return condominoDAO.getLista("Massao").get(0);
+    }    
+  }
+  
+  private void removerCondominoTeste(){
+    CondominoDAO condominoDAO = new CondominoDAO();
+    if (!condominoDAO.getLista("Massao").isEmpty()) {
+      Condomino condomino = condominoDAO.getLista("Massao").get(0);
+      condominoDAO.deletar(condomino);
+    }    
+  }
+  
+  private void removerCondominioTeste(){
+    CondominioDAO condominioDAO = new CondominioDAO();
+    if (!condominioDAO.getLista("condominio_teste", "ativo").isEmpty()) {
+      Condominio condominio = condominioDAO.getLista("condominio_teste", "ativo").get(0);
+      condominioDAO.deletar(condominio);
     }    
   }
 }
